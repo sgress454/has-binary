b8b71d41731dba6a03e5bee623e4203be7af8b8e
Guillermo Rauch <rauchg@gmail.com>
2015-11-18 13:02:20 -0800

    fix `global.isBuffer` usage [@tonetheman]

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function hasBinary(data) {
   function _hasBinary(obj) {
     if (!obj) return false;
 
-    if ( (global.Buffer && global.Buffer.isBuffer(obj)) ||
+    if ( (global.Buffer && global.Buffer.isBuffer && global.Buffer.isBuffer(obj)) ||
          (global.ArrayBuffer && obj instanceof ArrayBuffer) ||
          (global.Blob && obj instanceof Blob) ||
          (global.File && obj instanceof File)
